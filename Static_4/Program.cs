24d3075d1bb61652acc8ff4316dd7f50f2710637
Ratimir <ra0953029364@gmail.com>
2020-01-19 16:24:46 +0200

    Добавьте файлы проекта.

---
+++ Static_4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Static_4
+{
+    static class StringExtension
+    {
+        public static string MySubstring(this string str, int beg, int len)
+        {
+            char[] res = new char[len];
+            for (int i = 0; i < len; i++)
+                res[i] = str[i + beg];
+            return new string(res);
+        }
+        public static int MyIndexOf(this string str, string value)
+        {
+            for (int i = 0; i < str.Length - value.Length + 1; i++)
+                if (str.MySubstring(i, value.Length) == value)
+                    return i;
+            return -1;
+        }
+        public static string MyReplace(this string str, string oldValue, string newValue)
+        {
+            int i;
+            while (str.MyIndexOf(oldValue) >= 0)
+            {
+                i = str.MyIndexOf(oldValue);
+                str = str.Remove(i, oldValue.Length);
+                str = str.Insert(i, newValue);
+            }
+            return str;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello world!";
+            Console.WriteLine(str.MySubstring(6, 5));
+            Console.WriteLine(str.MyIndexOf("world!"));
+            Console.WriteLine(str.MyReplace("l", "I"));
+
+            Console.ReadKey();
+        }
+    }
+}
